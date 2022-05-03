68f344e4bc8af11ce01d3b5cd2bad2af941302d5
Maike <maike@mouts.info>
2022-05-03 19:56:35 -0300

    Ajuste nas seeds e utilização de migrations.

---
+++ database/migrations/2022_02_01_143203_alimentar_seeds.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MigrationUsuarioAddCampos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Artisan::call('db:seed', [
+            '--force' => true,
+            '--class' => Illuminate\Database\Seeder\SeedAuthSeeder::class,
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
